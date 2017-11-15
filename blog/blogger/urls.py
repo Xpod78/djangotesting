df8d0e8b93d95772f4ef56f7eb19d87e1aa9d573
Eric Isom <ericisom887@gmail.com>
2017-11-15 20:36:53 +0000

    Started login page - signup template and views changed

--- blog/blogger/urls.py
+++ blog/blogger/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.LoginView.as_view(), name='login_page'),
-    url(r'^signup/', views.SignUpView.as_view(), name='signup_page'),
+    url(r'^signup/', views.signup, name='signup_page'),
 ]
--- blog/blogger/views.py
+++ blog/blogger/views.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.views.generic import View, ListView, FormView
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('pass')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('blogger/index.html')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blogger/signup.html', {'form': form})
 class UserPageView(View):
     
     def get(self, request):
