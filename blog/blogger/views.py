6bed02f2c46df9bab7b0177da416a177401c8fe5
Eric Isom <ericisom887@gmail.com>
2017-11-20 13:31:47 +0000

    Cleaned up code, fixed new entry error

--- blog/blogger/views.py
+++ blog/blogger/views.py
@@ -14,20 +14,20 @@ from .models import Post
 from .forms import PostForm
 
 def new_entry(request):
+    form = PostForm(request.POST or None)
     if request.method == "POST":
-        form = PostForm()
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = User
+            post.author = request.user
             post.date_created = timezone.now()
             post.date_last_modified = timezone.now()
             post.save()
-            return redirect('post_page', pk=post.pk) 
+            return redirect('blogger/post_page', pk=post.pk) 
     else:
         form = PostForm()
     return render(request, 'blogger/new_entry.html', {'form': form})
 	    
-    return render(request, 'blogger/new_entry.html', {'form': form})
+    return render(request, 'blogger/index.html', {'form': form})
 
 def post_page(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -37,16 +37,6 @@ def post_list(request):
     posts = Post.objects.filter(date_created__lte=timezone.now()).order_by('date_last_modified')
     return render(request, 'blogger/all_blogs.html', {'posts': posts})
 
-def add_post(request):
-    form = PostForm(request.POST or None)
-    if form.is_valid():
-        post = form.save(commit=False)
-        post.author = request.user
-        post.save()
-        return redirect(post)
-    return render_to_response('blogger/new_entry.html', 
-                              { 'form': form }, request)
-
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -54,20 +44,11 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('pass')
-	    first_name = form.cleaned_data.get('firstname')
-	    last_name = form.cleaned_data.get('lastname')
- 	    email_address = form.cleaned_data.get('email')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('blogger/index.html')
     else:
         form = UserCreationForm()
     return render(request, 'blogger/signup.html', {'form': form})
-class UserPageView(View):
-    
-    def get(self, request):
-        return render(request, 'blogger/user.html')
 
 
-
-# Create your views here.
